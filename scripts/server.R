69b786734154e340e6247d1b68fda8800461a9c6
David Fishman <davjfish@gmail.com>
2020-04-28 13:15:49 -0300

    altering structure for serving with shiny-server

--- scripts/server.R
+++ scripts/server.R
@@ -48,7 +48,7 @@ server <- function(input, output, session) {
   }
   output$versionCheck <- renderUI(HTML(updMsg))
   
-  source("./getHelp.R")
+  source("./scripts/getHelp.r")
   
   output$getHelp<-renderUI({
     getHelp()
