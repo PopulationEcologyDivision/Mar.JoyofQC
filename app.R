a6081a2eba2b9abec523b049ae609f36c8529300
David Fishman <davjfish@gmail.com>
2020-04-28 13:48:23 -0300

    altering structure for serving with shiny-server

--- app.R
+++ app.R
@@ -1,9 +1,5 @@
 setwd(".")
 ui = source('./scripts/ui.R', local = TRUE)$value
-server = function(input, output) {
-	  ## rwData
-	  source('./scripts/server.R', local = TRUE)$value
-	}
-
+server = source('./scripts/server.R', local = TRUE)$value
 
 shinyApp(ui=ui, server=server)
